d9b057026e4ff2addf9a154f64a8416f42dad716
Lee Chuan Xin <chuanxin.lee@gmail.com>
2017-02-28 07:08:59 +0800

    Added conditions for keying input entries

--- FreeCodeCamp-Calculator/js/main.js
+++ FreeCodeCamp-Calculator/js/main.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// test 1.22 + 5.6 x 1.3
+
 // Buttons
 var buttons = (function() {
 	// cache DOM
@@ -23,21 +27,23 @@ var buttons = (function() {
 	});
 })();
 
+
 // Display
 var display = (function() {
 	// cache DOM
-	var input = document.querySelector('.input').querySelector('span');
-	var answer = document.querySelector('.answer').querySelector('span');
+	var input = document.querySelector('.input'),
+		answer = document.querySelector('.answer');
 
 	// init variables
-	var inputText = '';
-	var answerText = 0;
-	var limitWarning = 'Limit Reached!';
-	var maxDisplayLength = 12;
+	var inputText = '',
+		answerNum = 0,
+		answerText = 'ans'.toUpperCase(),
+		limitWarning = 'Limit Reached!'.toUpperCase(),
+		maxDisplayLength = 12;
 
 	// init timeout
-	var inputWarningTimeout;
-	var answerWarningTimeout;
+	var inputWarningTimeout,
+		answerWarningTimeout;
 
 	// render
 	renderAnswer();
@@ -45,62 +51,81 @@ var display = (function() {
 
 	// add input
 	function addInput(newInput) {
-		if (input.textContent.length >= maxDisplayLength) {
-			showWarning(input, inputText);
+		var previousInput = inputText[inputText.length - 1],
+			divideLastIndex = inputText.lastIndexOf('/'),
+			minusLastIndex = inputText.lastIndexOf('-'),
+			plusLastIndex = inputText.lastIndexOf('+'),
+			timesLastIndex = inputText.lastIndexOf('x'),
+			operatorLastIndexArr = [divideLastIndex, minusLastIndex, plusLastIndex, timesLastIndex];
+
+		// init basic conditions
+		var hasDecimalPointAfterLastOperator = (inputText.lastIndexOf('.') > Math.max.apply(null, operatorLastIndexArr)),
+			isDecimalPointOrOperator = function(str) {
+				return util.isDecimalPoint(str) || util.isOperator(str);
+			};
+			
+		// init complex conditions
+		var digitAfterAnswer = util.isAnswer(inputText) && (util.isNumber(newInput) || util.isDecimalPoint(newInput)),
+			multipleDecimalPoints = util.isDecimalPoint(newInput) && hasDecimalPointAfterLastOperator,
+			notNumberAfterDecimalPoint = util.isDecimalPoint(previousInput) && !util.isNumber(newInput),
+			notNumberBeforeDecimalPoint = isDecimalPointOrOperator(newInput) && isDecimalPointOrOperator(previousInput),
+			operatorAfterOperator = util.isOperator(previousInput) && util.isOperator(newInput),
+			rejectedConditions = digitAfterAnswer || multipleDecimalPoints || notNumberBeforeDecimalPoint || notNumberAfterDecimalPoint || operatorAfterOperator;
+
+		if (!rejectedConditions) {
+			renderInput(inputText + newInput);
 		}
-		else if (!input.classList.contains('warning')) {
-			inputText += newInput;
-			console.log(inputText.length);
-			renderInput();
-		}
+	};
+
+	// clear all
+	function clearAll() {
+		inputText = '';
+		answerNum = 0;
+
+		renderInput();
+		renderAnswer();
 	};
 
 	// clear entry
 	function clearEntry() {
 		inputText = inputText.slice(0, inputText.length - 1);
 		renderInput();
-	}
-
-	// clear input
-	function clearAll() {
-		inputText = '';
-		answerText = 0;
-		renderAnswer();
-		renderInput();
 	};
 
 	// equal
 	function equal() {
-		// evaluates only if last input entry is number
-		if (!isNaN(parseInt(inputText[inputText.length - 1]))) {
+		var previousInput = inputText[inputText.length - 1];
+
+		// evaluates only if last input entry is number or answer
+		if (util.isNumber(previousInput) || util.isAnswer(inputText)) {
 			var timesRegex = /x/g;
-			var evaluatedAnswer = eval(inputText.replace(timesRegex, "*")).toString();
-			var currentDisplay = answer.textContent;
-			
-			inputText = evaluatedAnswer;
 
-			renderAnswer(evaluatedAnswer);
-			
+			// convert "ANS" string to actual number for calc
+			if (inputText.indexOf(answerText) == 0) {
+				inputText = answerNum.toString() + inputText.slice(answerText.length);
+			}
 
-			if (answer.textContent.length > maxDisplayLength) {
-				console.log('SHOW WARNING');
-				showWarning(answer, currentDisplay);
-			}
-			else {
-				renderInput();
-			}
+			// evaluates answer from converted input text
+			renderAnswer(eval(inputText.replace(timesRegex, "*")).toString());
+
+			renderInput(answerText);
 		}
+	};
+
+	// get answer text
+	function getAnswerText() {
+		return answerText;
 	};
 
 	// hide warning
 	function hideWarning(element, elementDisplay) {
-		if (element.parentNode.classList.contains('input')) {
+		if (element.classList.contains('input')) {
 			inputWarningTimeout = setTimeout(function() {
 				if (element.classList.contains('warning')) {
 					element.classList.remove('warning');
 				}
 
-				renderInput(elementDisplay);
+				input.textContent = elementDisplay;
 			}, 1000);
 		}
 		else {
@@ -109,36 +134,52 @@ var display = (function() {
 					element.classList.remove('warning');
 				}
 
-				renderAnswer(elementDisplay);
+				answer.textContent = elementDisplay;
 			}, 1000);
+		}
+	};
+
+	// render answer
+	function renderAnswer(str) {
+		var currentAnswerDisplay = answer.textContent,
+			nextAnswerDisplay = str || answerNum.toString();
+
+		if (nextAnswerDisplay.length <= maxDisplayLength) {
+			answerNum = Number(nextAnswerDisplay);
+			answer.textContent = nextAnswerDisplay;
+		}
+		else if (!answer.classList.contains('warning')) {
+			showWarning(answer, currentAnswerDisplay);
 		}
 	};
 
 	// render input
 	function renderInput(str) {
-		input.textContent = str || inputText;
-	};
+		var currentInputDisplay = input.textContent,
+			nextInputDisplay = str || inputText;
 
-	// render answer
-	function renderAnswer(str) {
-		answer.textContent = str || answerText;
-
-		if (answer.textContent == '') {
-			answer.textContent += '0';
+		if (nextInputDisplay.length <= maxDisplayLength) {
+			inputText = nextInputDisplay;
+			input.textContent = inputText;
+		}
+		else if (!input.classList.contains('warning')) {
+			showWarning(input, currentInputDisplay);
 		}
 	};
 
 	// show warning
 	function showWarning(element, elementDisplay) {
-		element.className = 'warning';
+		element.className += ' warning';
 
-		if (element.parentNode.classList.contains('input')) {
+		if (element.classList.contains('input')) {
 			clearTimeout(inputWarningTimeout);
-			renderInput(limitWarning.toUpperCase());
+
+			input.textContent = limitWarning;
 		}
 		else {
 			clearTimeout(answerWarningTimeout);
-			renderAnswer(limitWarning.toUpperCase());
+
+			answer.textContent = limitWarning;
 		}
 
 		hideWarning(element, elementDisplay);
@@ -148,6 +189,39 @@ var display = (function() {
 		addInput: addInput,
 		clearEntry: clearEntry,
 		clearAll: clearAll,
-		equal: equal 
+		equal: equal,
+		getAnswerText: getAnswerText
 	};
 })();
+
+
+// Utility 
+var util = (function() {
+	// checks if string is answer text
+	function isAnswer(str) {
+		return (str == display.getAnswerText());
+	};
+
+	// checks if string is decimal point
+	function isDecimalPoint(str) {
+		return (str == '.');
+	};
+
+	// checks if string is number
+	function isNumber(str) {
+		return !isNaN(Number(str));
+	};
+
+	// checks if string is operator
+	function isOperator(str) {
+		return (str == 'x' || str == '/' || str == '+' || str == '-');
+	};
+
+	return {
+		isAnswer: isAnswer,
+		isDecimalPoint: isDecimalPoint,
+		isNumber: isNumber,
+		isOperator: isOperator
+	};
+})();
+	
